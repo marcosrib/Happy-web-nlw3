edc3af02d3f1787e3b7b4e23391c34331188c59b
marcos <marcos.rib.sousa@gmail.com>
2020-10-15 21:14:27 -0300

    abstrations components

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,36 +1,21 @@
 import React from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
 
-import mapMarkerImg from '../images/map-marker.svg';
+
+import SideBar from '../components/SideBar';
+
+import happyMapIcon from '../utils/mapIcon';
 
 import '../styles/pages/create-orphanage.css';
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
 
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+     <SideBar />
 
       <main>
         <form className="create-orphanage-form">
@@ -105,3 +90,4 @@ export default function CreateOrphanage() {
 }
 
 // return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
+
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup  } from 'react-leaflet'
-import Leaflet from 'leaflet';
+
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanage-map.css'
-import 'leaflet/dist/leaflet.css'
 
-const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg,
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
-})
+
+import happyMapIcon from '../utils/mapIcon';
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -38,7 +33,7 @@ const OrphanagesMap: React.FC = () => {
         >
           <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
           <Marker 
-           icon={mapIcon}
+           icon={happyMapIcon}
            position={[-23.498021,-46.8050558]}
           >
             <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
